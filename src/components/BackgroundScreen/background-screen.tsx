e7f35065687ede017659253804a126cbe7d1f3c7
Pankaj Patil <pankaj.p@applicationnexus.com>
2019-09-05 13:55:34 +0530

    Added background screen component

---
+++ src/components/BackgroundScreen/background-screen.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from './background-screen.module.scss'
+
+export default class BackgroundScreen extends React.Component {
+    render() {
+        return <div className={style.background}> </div>
+    }
+}
